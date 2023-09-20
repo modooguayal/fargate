d0c1f743fc5da3b11b1aa2ff9a3729a96adc335c
jeffgcuadras <jeffgcuadras@gmail.com>
2023-09-20 15:50:50 +0200

    First commit

---
+++ main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  region = "us-east-1" # Cambia esto según tu región preferida
+}
+
+resource "aws_ecs_cluster" "nginx_cluster" {
+  name = "nginx-cluster"
+}
+
+resource "aws_ecs_task_definition" "nginx" {
+  family                   = "nginx"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name  = "nginx"
+    image = "nginx:latest"
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role_nginx"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name = "ecs_tasks_sg"
+  description = "Allow inbound traffic to ECS tasks"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+resource "aws_ecs_service" "nginx" {
+  name            = "nginx-service"
+  cluster         = aws_ecs_cluster.nginx_cluster.id
+  task_definition = aws_ecs_task_definition.nginx.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets = ["subnet-1ddbd557","subnet-f3e4a1dd","subnet-ac179592","subnet-ada4daca","subnet-9686fdca","subnet-dffacad0"] # Cambia esto con tus subredes
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_tasks.id]
+  }
+}
